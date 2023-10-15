04d0939470db016f1db878b1b01395e432f8151c
soh256 <sohkengnerynald@gmail.com>
2023-10-15 10:25:38 +0100

    travail sur la page create years
    on ameliore les boutton

--- resources/views/livewire/create-new-y.blade.php
+++ resources/views/livewire/create-new-y.blade.php
@@ -1,8 +1,16 @@
-<div class="p-3 shadow-sn bg-white-600">
+<div class="bg-white-900 p-3 shadow-sm ">
     <form method="POST">
         <div class="p-5">    
-            <x-jet-label value="{{__('Libellé de l année')}}"/>
+            <label value="{{__('Libellé de l année')}}">Libellé de l'année</lablel>
             <input type="text" class="block mt-1 rounded-sm border-gray-300 min-w-full">
+        </div>
+        <div class="flex justify-between items-center p-5">
+            <button class="bg-red-600 rounded-sm p-2 text-white" >
+                annuler
+            </button>
+            <button class="bg-green-600 rounded-sm p-2 text-white">
+                ajouter
+            </button>
         </div>
     </form>
 
