eef75570e6912a7d6c06455a92aec3d69d8e3939
soh256 <123369400+soh256@users.noreply.github.com>
2023-10-23 04:38:19 +0200

    premieres sauvegarde en base de donnéee
    creation du crud du model date

--- app/Http/Livewire/CreateNewY.php
+++ app/Http/Livewire/CreateNewY.php
@@ -3,9 +3,33 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\SchoolYear;
+use Carbon\Carbon;
 
 class CreateNewY extends Component
 {
+    public $libelle;
+    
+    public function save_Year(SchoolYear $school_years){
+        $this->validate([
+            'libelle' => 'string|required|unique:school_years,School_year'
+        ]);
+        // dd($this->libelle);
+        try {
+            //test pour l'enregistrement de l'année
+        $current_Year = Carbon::now()->format('Y');
+        $school_years->School_year = $this->libelle;
+        $school_years->current_year = $current_Year;
+        $school_years->save();
+        if ($school_years) {
+            $libelle = "";
+        }
+        return redirect()->back()->with('succes','l\'année à bien été ajoutée');
+        } catch (Exception $e) {
+            //code qui s'effectue si l'enregistrement ne marche pas
+        }
+        
+    }
     public function render()
     {
         return view('livewire.create-new-y');
--- resources/views/livewire/create-new-y.blade.php
+++ resources/views/livewire/create-new-y.blade.php
@@ -1,14 +1,17 @@
 <div class="bg-white-900 p-3 shadow-sm ">
-    <form method="POST">
-        <div class="p-5">    
-            <label value="{{__('Libellé de l année')}}">Libellé de l'année</lablel>
-            <input type="text" class="block mt-1 rounded-sm border-gray-300 min-w-full">
+    <form method="POST" wire:submit.prevent="save_Year">
+        @csrf
+        @method('post')
+        <div class="p-5">
+            <label value="{{__('Libellé de l année')}}">Libellé de l'année</label>
+            @er
+            <input type="text" class="block mt-1 rounded-sm border-gray-300 min-w-full" wire:model='libelle'>
         </div>
         <div class="flex justify-between items-center p-5">
-            <button class="bg-red-600 rounded-sm p-2 text-white" >
+            <button class="bg-red-600 rounded-sm p-3 text-white text-sm" >
                 annuler
             </button>
-            <button class="bg-green-600 rounded-sm p-2 text-white">
+            <button type="submit" class="bg-green-600 rounded-sm p-3 text-white text-sm">
                 ajouter
             </button>
         </div>
