1f622b5c6fb083fabe89d061d0d8b2b40231ebad
soh256 <123369400+soh256@users.noreply.github.com>
2023-10-23 06:54:45 +0200

    suite du travail
    crud année

--- app/Http/Livewire/CreateNewY.php
+++ app/Http/Livewire/CreateNewY.php
@@ -5,6 +5,9 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\SchoolYear;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+
 
 class CreateNewY extends Component
 {
@@ -12,22 +15,30 @@ class CreateNewY extends Component
     
     public function save_Year(SchoolYear $school_years){
         $this->validate([
-            'libelle' => 'string|required|unique:school_years,School_year'
+            'libelle' => 'String|required|unique:school_years,School_year'
         ]);
         // dd($this->libelle);
         try {
             //test pour l'enregistrement de l'année
         $current_Year = Carbon::now()->format('Y');
-        $school_years->School_year = $this->libelle;
-        $school_years->current_year = $current_Year;
-        $school_years->save();
-        if ($school_years) {
-            $libelle = "";
-        }
-        return redirect()->back()->with('succes','l\'année à bien été ajoutée');
+        $tchek = schoolYear::where('current_year', $current_Year)->get();
+        $existe = $tchek->count();
+        
+        if($existe > 1){
+            return redirect()->back()->with('error', 'l\'année existe déjà!!');
+        }else{
+            $school_years->School_year = $this->libelle;
+            $school_years->current_year = $current_Year;
+            $school_years->save();
+            if ($school_years) {
+                $libelle = "";
+            }
+            return redirect()->back()->with('succes','l\'année à bien été ajoutée');
+           } 
         } catch (Exception $e) {
-            //code qui s'effectue si l'enregistrement ne marche pas
-        }
+                //code qui s'effectue si l'enregistrement ne marche pas
+            }
+        
         
     }
     public function render()
--- resources/views/settings/Create-Sy.blade.php
+++ resources/views/settings/Create-Sy.blade.php
@@ -1,3 +1,8 @@
+
+@php
+use Illuminate\Support\Facades\Session;
+@endphp
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
